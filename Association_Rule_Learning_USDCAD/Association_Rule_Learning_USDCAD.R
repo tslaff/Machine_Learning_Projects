1d19a5b8b748a8ed56cd77a69bc1686b646cb002
Tad <t.slaff@inovancetech.com>
2016-04-20 17:35:50 -0400

    Added data link and multiple plots

--- Association_Rule_Learning_USDCAD/Association_Rule_Learning_USDCAD.R
+++ Association_Rule_Learning_USDCAD/Association_Rule_Learning_USDCAD.R
@@ -8,9 +8,14 @@ library(e1071)
 # The charting functions we will use
 install.packages("ggplot2")
 library(ggplot2)
+# Allow us to download data directly from dropbox
+install.packages("repmis")
+library(repmis)
 
-# Grab the data from this dropbox link
-Data<-USDCAD
+# Grab the data directly from my dropbox account
+filename <-"USDCAD.csv"
+mykey <- "xlrbbnm465jlf6z"
+Data<-source_DropboxData(filename,key=mykey, sep=",", header=TRUE)
 
 # Calculate our indicators
 CCI20<-CCI(Data[,3:5],n=20)
@@ -32,11 +37,11 @@ Class<-ifelse(Price > 0 ,"UP","DOWN")
 # Remove oldest data point to line up with indicators
 Class<-Class[-1]
 DataSet<-data.frame(Indicators,Class)
-DataSet<-na.exclued(DataSet)
+DataSet<-na.exclude(DataSet)
 
 # Break int training, test and validation sets
 Breakpoint_Test = (0.6)*nrow(DataSet)
-Breakpoint_Val = (0.8)*nrow(lineDataSet)
+Breakpoint_Val = (0.8)*nrow(DataSet)
 Training<-DataSet[1:Breakpoint_Test,]
 Test<-DataSet[(Breakpoint_Test+1):Breakpoint_Val,]
 Validation<-DataSet[(Breakpoint_Val+1):nrow(DataSet),]
@@ -48,23 +53,37 @@ NB<-naiveBayes(Class ~ RSI3 + CCI20 + DEMA10c, data=Training)
 table(predict(NB,Test,type="class"),Test[,4],dnn=list('predicted','actual'))
 
 # Now let's see where the algorithm went long and where it went short
-# First build our data set of its predictions, and whether it was correct or incorrect
+# First build our data set of whether it predicted long or short over the training set
+TrainingPredictions<-predict(NB,Training,type="class")
+TrainingData<-data.frame(Training,TrainingPredictions)
+
+
+# Build a data set of where the algorithm was correct or incorrect
 TestPredictions<-predict(NB,Test,type="class")
 TestCorrect<-ifelse(TestPredictions==Test[,4],"Correct","Incorrect")
 TestData<-data.frame(Test,TestPredictions,TestCorrect)
 
 # Build two sets of plots: one for predictions and one for accuracy
 # For the predictions:
-ggplot(TrainingData,aes(x=CCI20,fill=TrainingPredictions))+
+Training_colnames<-colnames(TrainingData)
+Prediction_Plots<-list()
+for(i in 1:(length(TrainingData)-2)){
+  Prediction_Plots[[i]]<-ggplot(TrainingData,aes_string(x=Training_colnames[i],fill="TrainingPredictions"))+
   geom_histogram(binwidth=15,position="fill")+
-  labs(title="Training Predictions: CCI", x = "20-Period CCI", y= "Up/Down Ratio",fill="Predictions")+
+  labs(title="Training Predictions", y= "Up/Down Ratio",fill="Predictions")+
   scale_fill_manual(values=c("#FF6737","#00B204"))
+}  
 
 # For the accuracy:
-ggplot(TestData,aes(x=RSI3,fill=TestCorrect))+
-  geom_histogram(binwidth=5,position="fill")+
-  labs(title="Test Accuracy: RSI", x = "3-Period RSI", y= "Correct/Incorrect Ratio",fill="Accuracy")+
-  scale_fill_manual(values=c("#0066FF","#FF4747"))
+Test_colnames<-colnames(TestData)
+Accuracy_Plots<-list()
+for(i in 1:(length(TestData)-3)){
+      bin <-(max(TestData[,i])-min(TestData[,i]))/20
+      Accuracy_Plots[[i]]<-ggplot(TestData,aes_string(x=Test_colnames[i],fill="TestCorrect"))+
+      geom_histogram(binwidth=bin,position="fill")+
+      labs(title="Test Accuracy", y= "Correct/Incorrect Ratio",fill="Accuracy")+
+      scale_fill_manual(values=c("#0066FF","#FF4747"))
+} 
 
 # We then manually build the rules by looking for areas where there was a majority of the prediciton direction and high accuracy
 # Here are the long rules I built:
